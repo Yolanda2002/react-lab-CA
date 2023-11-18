1cbebd220a2bf59e95002ab61c62a4e44cf7fb32
Jia Yang <20104736@mail.wit.ie>
2023-11-18 23:40:22 +0000

    Add Filter to Person Page

--- src/components/personCard/index.js
+++ src/components/personCard/index.js
@@ -1,35 +1,24 @@
-
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
 
-const StyledCard = styled(Card)({
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between', 
-  });
-  
-  const StyledCardMedia = styled(CardMedia)({
-    height: 400, 
-    backgroundSize: 'cover', 
-  });
 export default function PersonCard({ person }) {
-    return (
-        <StyledCard>
-          <StyledCardMedia
-            image={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
-            alt={person.name}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="div">
-              {person.name}
-            </Typography>
-            {/* 后緒添加更多的信息 */}
-          </CardContent>
-        </StyledCard>
-      );
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="340" // 可以根据需要调整
+        image={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
+        alt={person.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          {person.name}
+        </Typography>
+        {/* 可以添加更多的个人信息 */}
+      </CardContent>
+    </Card>
+  );
 }
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,13 +1,16 @@
-
 import React, { useEffect, useState } from 'react';
-import { getPeople } from '../api/tmdb-api'; 
+import { getPeople } from '../api/tmdb-api';
 import Grid from '@mui/material/Grid';
-import PersonCard from '../components/personCard';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
+import PersonCard from '../components/personCard'; // 假设你已经有了这个组件
 
 export default function PeoplePage() {
   const [people, setPeople] = useState([]);
+  const [genderFilter, setGenderFilter] = useState('all'); // 'male', 'female', 'all'
+  const [creditsFilter, setCreditsFilter] = useState(0);
 
   useEffect(() => {
     getPeople().then(data => {
@@ -15,18 +18,39 @@ export default function PeoplePage() {
     });
   }, []);
 
+  // filter setting
+  const filteredPeople = people.filter((person) => {
+    const genderMatch = genderFilter === 'all' || person.gender === (genderFilter === 'male' ? 2 : 1);
+    const creditsMatch = person.known_for_department === 'Acting' && person.known_for.length >= creditsFilter;
+    return genderMatch && creditsMatch;
+  });
+
   return (
-    <Container>
-      <Typography variant="h3" component="h1" gutterBottom>
-        Famous People
-      </Typography>
+    <>
+      <Box sx={{ my: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          People
+        </Typography>
+        <TextField
+          select
+          label="Gender"
+          value={genderFilter}
+          onChange={(e) => setGenderFilter(e.target.value)}
+          sx={{ mr: 2 }}
+        >
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="male">Male</MenuItem>
+          <MenuItem value="female">Female</MenuItem>
+        </TextField>
+        
+      </Box>
       <Grid container spacing={4}>
-        {people.map((person) => (
+        {filteredPeople.map((person) => (
           <Grid item key={person.id} xs={12} sm={6} md={4} lg={3}>
             <PersonCard person={person} />
           </Grid>
         ))}
       </Grid>
-    </Container>
+    </>
   );
 }
