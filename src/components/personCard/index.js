f3043d8ac411afe29b20b99f17f2ce0c024fbb8e
Jia Yang <20104736@mail.wit.ie>
2023-11-18 23:50:28 +0000

    Link from People to Movies

--- src/components/personCard/index.js
+++ src/components/personCard/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom'; // 导入 RouterLink 组件
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 export default function PersonCard({ person }) {
   return (
     <Card>
       <CardMedia
         component="img"
-        height="340" // 可以根据需要调整
+        height="340"
         image={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
         alt={person.name}
       />
@@ -17,7 +19,15 @@ export default function PersonCard({ person }) {
         <Typography gutterBottom variant="h6" component="div">
           {person.name}
         </Typography>
-        {/* 可以添加更多的个人信息 */}
+        {/* 渲染电影链接 */}
+        {person.known_for && person.known_for.map((movie) => (
+          <Typography key={movie.id} variant="body2">
+            {/* 使用 RouterLink 作为 Link 组件的 'component' 属性以提供路由导航 */}
+            <Link component={RouterLink} to={`/movies/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </Typography>
+        ))}
       </CardContent>
     </Card>
   );
